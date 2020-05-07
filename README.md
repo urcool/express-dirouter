8b4cfeb9c24fd32a38b1539870b3cad3026674be
835995175 <835995175@qq.com>
2020-05-07 14:24:29 +0800

    commit init

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # express-dirouter
-依据 controller/service 的目录结构自动生成 express-router 路由配置以及可通过this.service[filename]调用的方法集合
+ 依据 controller/service 的目录结构自动生成 express-router 路由配置( restful风格 )以及可通过this.service[filename]调用的方法集合
+
+# 安装
+```
+    npm i -S express-dirouter
+```
+## 1.目录结构
+```
+  ├─app.js
+  ├─controller
+  │     └─test.js
+  └─service
+        └─one.js
+```
+## 2.具体应用
+app.js
+```javascript
+const express = require('express')
+const app = express()
+const port = 3000
+const DiRouter = require('express-dirouter')
+// 默认配置
+app.use(new DiRouter())
+/*
+ 自定义配置
+ 当controller/service目录在src目录下
+ app.use(new DiRouter({
+    controllerDir: 'src/controller'
+    serviceDir: 'src/service'
+ }))
+*/
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+```
+
+service/one.js
+
+```javascript
+// 例子导出了一个es6对象
+module.exports = {
+    async bb(){
+        await 12;
+        return 'bb'
+    }
+}
+```
+
+controller/test.js
+```javascript
+// 例子导出了一个es6对象
+module.exports = {
+    async get(req, res) {
+        const result = await this.service.one.bb()
+        // result 会得到service例子返回的值 ‘bb’
+        res.send(result)
+    },
+    post(req, res) {
+        res.send('post is ok')
+    },
+    put(req, res) {
+        const { id } = req.params
+        res.send( id )
+    }
+}
+```
+
+## 3.运行结果
+
+ `npm i -S express` 安装 express
+
+ `node app.js` 运行程序
+
+| 请求方式 | 路由     | 返回结果     |
+|----------|----------|--------------|
+| GET      | /test    | 'bb'         |
+| POST     | /test    | 'post is ok' |
+| PUT      | /test/12 | 12           |
+
+## 4. 关于 controller 导出对象的命名与restful请求对应值
+
+| key   | method | router    |
+|-------|--------|-----------|
+| get   | GET    | /test     |
+| post  | POST   | /test     |
+| show  | GET    | /test/:id |
+| put   | PUT    | /test/:id |
+| patch | PATCH  | /test/:id |
+| del   | DELETE | /test/:id |
+
---
+++ index.js
@@ -0,0 +1,80 @@
+const fs = require('fs')
+const path = require('path')
+const router = require('express').Router()
+
+const log = (...arg) => console.log(`[${new Date().toLocaleString()}]`, ...arg)
+const extnameGet = filePath => fs.statSync(filePath).isDirectory() ? 'dir' : path.extname(filePath)
+
+module.exports = class {
+    constructor(options = {}) {
+        this.controllerDir = options.controllerDir || 'controller'
+        this.serviceDir = options.serviceDir || 'service'
+        try {
+            this.service = this.serviceBuilder(this.serviceDir.split('/'))
+            this.controllerBuilder(this.controllerDir.split('/'))
+        } catch (e) {
+            throw new Error(e.message)
+        }
+        return router
+    }
+    controllerBuilder(controllerDir) {
+        const baseDir = path.join(__dirname, ...controllerDir)
+        const files = fs.readdirSync(baseDir)
+        const isFun = f => typeof f === 'function'
+        files.forEach(file => {
+            const filePath = path.join(baseDir, file)
+            const extname = extnameGet(filePath)
+            if (extname === '.js') {
+                const instance = require(filePath)
+                if (typeof instance !== 'object') {
+                    return false
+                }
+                const file = filePath.substring(__dirname.length + this.controllerDir.length + 1)
+                const routerPath = file.split(path.sep).join('/').substring(0, file.length - 3)
+                if (isFun(instance.get)) {
+                    router.get(routerPath, instance.get.bind(this))
+                }
+                if (isFun(instance.post)) {
+                    router.post(routerPath, instance.post.bind(this))
+                }
+                if (isFun(instance.put)) {
+                    router.put(routerPath + '/:id', instance.put.bind(this))
+                }
+                if (isFun(instance.patch)) {
+                    router.patch(routerPath + '/:id', instance.patch.bind(this))
+                }
+                if (isFun(instance.show)) {
+                    router.get(routerPath + '/:id', instance.show.bind(this))
+                }
+                if (isFun(instance.del)) {
+                    router.delete(routerPath + '/:id', instance.del.bind(this))
+                }
+                log('loaded controller', filePath)
+            }
+            if (extname === 'dir') {
+                this.controllerBuilder([...controllerDir, file])
+            }
+        });
+    }
+    serviceBuilder(serviceDir, service = {}) {
+        const baseDir = path.join(__dirname, ...serviceDir)
+        const files = fs.readdirSync(baseDir)
+        files.forEach(file => {
+            const filePath = path.join(baseDir, file)
+            const extname = extnameGet(filePath)
+            const key = path.basename(filePath, '.js')
+            if (extname === '.js') {
+                const instance = require(filePath)
+                if (typeof instance !== 'object') {
+                    throw new Error(file + '\'s exports is not object')
+                }
+                log('loaded service', filePath)
+                service[key] = instance
+            }
+            if (extname === 'dir') {
+                service[key] = this.serviceBuilder([...serviceDir, file])
+            }
+        });
+        return service
+    }
+}
